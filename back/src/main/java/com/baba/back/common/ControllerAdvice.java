d6edf9f0fbfaaa8049889b7f4cac3a2afa19f8cf
SWpark <79205414+seong-wooo@users.noreply.github.com>
2023-01-27 11:58:38 +0900

    feat: 로그 파일 출력을 위한 xml 파일 생성 (#12)

--- back/src/main/java/com/baba/back/common/ControllerAdvice.java
+++ back/src/main/java/com/baba/back/common/ControllerAdvice.java
@@ -6,7 +6,8 @@ import com.baba.back.exception.AuthorizationException;
 import com.baba.back.exception.BadRequestException;
 import com.baba.back.exception.NotFoundException;
 import java.util.stream.Collectors;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -14,31 +15,32 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<ExceptionResponse> handleAuthenticationException(AuthenticationException exception) {
-        log.warn(exception.getMessage());
+        logger.warn(exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ExceptionResponse("인증에 실패했습니다."));
     }
 
     @ExceptionHandler(AuthorizationException.class)
     public ResponseEntity<ExceptionResponse> handleAuthorizationException(AuthorizationException exception) {
-        log.warn(exception.getMessage());
+        logger.warn(exception.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ExceptionResponse("권한이 없습니다."));
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ExceptionResponse> handleBadRequestException(BadRequestException exception) {
-        log.warn(exception.getMessage());
+        logger.warn(exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponse("잘못된 요청입니다."));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleNotFoundException(NotFoundException exception) {
-        log.warn(exception.getMessage());
+        logger.warn(exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionResponse("요청한 리소스를 찾을 수 없습니다."));
     }
 
@@ -50,20 +52,20 @@ public class ControllerAdvice {
                 .map(error -> String.format("%s: %s", ((FieldError) error).getField(), error.getDefaultMessage()))
                 .collect(Collectors.joining(", "));
 
-        log.warn(message);
+        logger.warn(message);
 
         return ResponseEntity.badRequest().body(new ExceptionResponse("잘못된 요청입니다."));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ExceptionResponse> handleRuntimeException(RuntimeException exception) {
-        log.warn(exception.getMessage());
+        logger.warn(exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponse("잘못된 요청입니다."));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleException(Exception exception) {
-        log.warn(exception.getMessage());
+        logger.warn(exception.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ExceptionResponse("처리할 수 없는 예외입니다."));
     }
 }
