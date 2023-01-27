2a8d34a7babd53e84a16e68ad79a3348a8a36c11
JunHyeongKim73 <rla5764v@gmail.com>
2023-01-27 17:11:42 +0900

    feat: `ContentDate` 클래스 구현

---
+++ back/src/test/java/com/baba/back/content/domain/ContentDateTest.java
@@ -0,0 +1,59 @@
+package com.baba.back.content.domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.baba.back.content.exception.ContentDateBadRequestException;
+import java.time.LocalDate;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class ContentDateTest {
+
+    public static Stream<Arguments> invalidContentDateAndNow() {
+        final LocalDate now = LocalDate.now();
+        return Stream.of(
+                Arguments.of(null, null, null),
+                Arguments.of(now.plusDays(1), now, now)
+        );
+    }
+
+    public static Stream<Arguments> invalidContentDateAndBirthday() {
+        final LocalDate now = LocalDate.now();
+        return Stream.of(
+                Arguments.of(null, null, null),
+                Arguments.of(now.minusYears(2).minusDays(1), now, now)
+        );
+    }
+
+    public static Stream<Arguments> validContentDateAndNowAndBirthday() {
+        final LocalDate now = LocalDate.now();
+        return Stream.of(
+                Arguments.of(now, now, now),
+                Arguments.of(now.minusYears(2), now, now)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidContentDateAndNow")
+    void contentDate는_now보다_미래일수_없다(LocalDate contentDate, LocalDate now, LocalDate birthday) {
+        assertThatThrownBy(() -> ContentDate.of(contentDate, now, birthday))
+                .isInstanceOf(ContentDateBadRequestException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidContentDateAndBirthday")
+    void contentDate는_birthday의_2년전까지_유효하다(LocalDate contentDate, LocalDate now, LocalDate birthday) {
+        assertThatThrownBy(() -> ContentDate.of(contentDate, now, birthday))
+                .isInstanceOf(ContentDateBadRequestException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("validContentDateAndNowAndBirthday")
+    void contentDate는_now보다_과거이고_birthday의_2년이내이다(LocalDate contentDate, LocalDate now, LocalDate birthday) {
+        assertThatCode(() -> ContentDate.of(contentDate, now, birthday))
+                .doesNotThrowAnyException();
+    }
+}
