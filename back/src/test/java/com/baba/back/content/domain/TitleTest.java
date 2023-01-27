9a1423397a8c5d927b55a927f14e909735d96f29
JunHyeongKim73 <rla5764v@gmail.com>
2023-01-27 17:10:56 +0900

    feat: `Title` 클래스 구현

---
+++ back/src/test/java/com/baba/back/content/domain/TitleTest.java
@@ -0,0 +1,29 @@
+package com.baba.back.content.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import com.baba.back.content.exception.TitleLengthBadRequestException;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+class TitleTest {
+
+    public static Stream<Arguments> invalidTitle() {
+        return Stream.of(
+                Arguments.of("a".repeat(11)),
+                Arguments.of("박".repeat(11))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidTitle")
+    @NullAndEmptySource
+    void 제목이_10자_초과면_예외를_던진다(String title) {
+        assertThatThrownBy(() -> new Title(title))
+                .isInstanceOf(TitleLengthBadRequestException.class);
+    }
+
+}
