f579968f84deb36b5259e250a335b71037ca6390
JunHyeongKim73 <rla5764v@gmail.com>
2023-01-27 18:08:01 +0900

    feat: `ImageSource` 클래스 구현

---
+++ back/src/main/java/com/baba/back/content/domain/ImageSource.java
@@ -0,0 +1,25 @@
+package com.baba.back.content.domain;
+
+import com.baba.back.content.exception.ImageSourceBadRequestException;
+import jakarta.persistence.Embeddable;
+import java.util.Objects;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+public class ImageSource {
+    private String imageSource;
+
+    public ImageSource(String imageSource) {
+        validateNull(imageSource);
+        this.imageSource = imageSource;
+    }
+
+    private void validateNull(String imageSource) {
+        if (Objects.isNull(imageSource)) {
+            throw new ImageSourceBadRequestException("ImageSource는 null일 수 없습니다.");
+        }
+    }
+}
---
+++ back/src/test/java/com/baba/back/content/domain/ImageSourceTest.java
@@ -0,0 +1,24 @@
+package com.baba.back.content.domain;
+
+import com.baba.back.content.exception.ImageSourceBadRequestException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+
+class ImageSourceTest {
+
+    @ParameterizedTest
+    @NullSource
+    void ImageSource는_null일수_없다(String imageSource) {
+        Assertions.assertThatThrownBy(() -> new ImageSource(imageSource))
+                .isInstanceOf(ImageSourceBadRequestException.class);
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    void ImageSource는_빈값일수_있다(String imageSource) {
+        Assertions.assertThatCode(() -> new ImageSource(imageSource))
+                .doesNotThrowAnyException();
+    }
+}
