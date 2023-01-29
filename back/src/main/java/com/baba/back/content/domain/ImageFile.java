be034adb425c4dadba08b949ea335423cde97231
JunHyeongKim73 <rla5764v@gmail.com>
2023-01-29 11:07:19 +0900

    feat: `ImageFile` 클래스 구현

---
+++ back/src/main/java/com/baba/back/content/domain/ImageFile.java
@@ -0,0 +1,20 @@
+package com.baba.back.content.domain;
+
+import com.baba.back.content.exception.ImageFileBadRequestException;
+import java.util.Objects;
+import org.springframework.web.multipart.MultipartFile;
+
+public class ImageFile {
+    private final MultipartFile file;
+
+    public ImageFile(MultipartFile file) {
+        validateImage(file);
+        this.file = file;
+    }
+
+    private void validateImage(MultipartFile file) {
+        if(!Objects.equals(file.getContentType(), "image/png")) {
+            throw new ImageFileBadRequestException("파일이 이미지 형식이 아닙니다.");
+        }
+    }
+}
---
+++ back/src/test/java/com/baba/back/content/domain/ImageFileTest.java
@@ -0,0 +1,32 @@
+package com.baba.back.content.domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.baba.back.content.exception.ImageFileBadRequestException;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+class ImageFileTest {
+
+    @Test
+    void 이미지가_아닌_파일을_받으면_예외를_던진다() {
+        // given
+        MultipartFile mockFile = new MockMultipartFile("photo", "file.png", "text/plain", "Spring Framework".getBytes());
+
+        // when & then
+        assertThatThrownBy(() -> new ImageFile(mockFile))
+                .isInstanceOf(ImageFileBadRequestException.class);
+    }
+
+    @Test
+    void 이미지_파일을_받으면_객체를_생성한다() {
+        // given
+        MultipartFile mockFile = new MockMultipartFile("photo", "file.png", "image/png", "Spring Framework".getBytes());
+
+        // when & then
+        assertThatCode(() -> new ImageFile(mockFile))
+                .doesNotThrowAnyException();
+    }
+}
