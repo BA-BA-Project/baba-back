adcaa47f354b511c12ff3a4af22b37ea9930321d
seong-wooo <seongwoo0513@uos.ac.kr>
2023-02-01 02:50:10 +0900

    feat: swagger ui 구성

--- back/src/main/java/com/baba/back/oauth/controller/MemberController.java
+++ back/src/main/java/com/baba/back/oauth/controller/MemberController.java
@@ -4,6 +4,10 @@ import com.baba.back.oauth.dto.MemberJoinRequest;
 import com.baba.back.oauth.dto.MemberJoinResponse;
 import com.baba.back.oauth.service.MemberService;
 import com.baba.back.oauth.support.Login;
+import com.baba.back.swagger.DefaultErrorResponses;
+import com.baba.back.swagger.OkResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,12 +16,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "멤버 관련 API")
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "멤버 생성 요청")
+    @DefaultErrorResponses
+    @OkResponse
     @PostMapping("/members")
     public ResponseEntity<MemberJoinResponse> joinMember(@RequestBody @Valid MemberJoinRequest request,
                                                          @Login String memberId) {
--- back/src/main/java/com/baba/back/oauth/controller/OAuthController.java
+++ back/src/main/java/com/baba/back/oauth/controller/OAuthController.java
@@ -2,18 +2,26 @@ package com.baba.back.oauth.controller;
 
 import com.baba.back.oauth.dto.TokenResponse;
 import com.baba.back.oauth.service.OAuthService;
+import com.baba.back.swagger.AllErrorResponses;
+import com.baba.back.swagger.OkResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "로그인 관련 API")
 @RestController
 @RequiredArgsConstructor
 public class OAuthController {
 
     private final OAuthService OAuthService;
 
+    @Operation(summary = "카카오 로그인 요청")
+    @AllErrorResponses
+    @OkResponse
     @GetMapping("/login/oauth2/code/kakao")
     public ResponseEntity<TokenResponse> signInKakao(@RequestParam("code") String code) {
         return ResponseEntity.ok(OAuthService.signInKakao(code));
