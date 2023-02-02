b568753d094447f1d3ff45017fa21f4f35bc46e5
JunHyeongKim73 <rla5764v@gmail.com>
2023-02-02 12:01:24 +0900

    refactor: Swagger에 필요한 어노테이션 추가

--- back/src/main/java/com/baba/back/content/controller/ContentController.java
+++ back/src/main/java/com/baba/back/content/controller/ContentController.java
@@ -4,6 +4,14 @@ import com.baba.back.content.dto.CreateContentRequest;
 import com.baba.back.content.dto.CreateContentResponse;
 import com.baba.back.content.service.ContentService;
 import com.baba.back.oauth.support.Login;
+import com.baba.back.swagger.BadRequestResponse;
+import com.baba.back.swagger.CreatedResponse;
+import com.baba.back.swagger.ForbiddenResponse;
+import com.baba.back.swagger.IntervalServerErrorResponse;
+import com.baba.back.swagger.NotFoundResponse;
+import com.baba.back.swagger.UnAuthorizedResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,12 +21,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "컨텐츠 관련 API")
 @RestController
 @RequiredArgsConstructor
 public class ContentController {
 
     private final ContentService contentService;
 
+    @Operation(summary = "컨텐츠 생성 요청")
+    @CreatedResponse
+    @BadRequestResponse
+    @UnAuthorizedResponse
+    @ForbiddenResponse
+    @NotFoundResponse
+    @IntervalServerErrorResponse
     @PostMapping("/album/{babyId}")
     public ResponseEntity<CreateContentResponse> createContent(@ModelAttribute @Valid CreateContentRequest request,
                                                                @Login String memberId,
