476cff63e91b63a6fac5ebfa653177d3614e7432
JunHyeongKim73 <rla5764v@gmail.com>
2023-02-04 13:39:32 +0900

    test: `ContentAcceptanceTest` s3 파일 업로드 요청 mocking

--- back/src/test/java/com/baba/back/content/acceptance/ContentAcceptanceTest.java
+++ back/src/test/java/com/baba/back/content/acceptance/ContentAcceptanceTest.java
@@ -5,7 +5,11 @@ import static com.baba.back.fixture.DomainFixture.멤버1;
 import static com.baba.back.fixture.DomainFixture.아기1;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
 import com.baba.back.AcceptanceTest;
 import com.baba.back.baby.repository.BabyRepository;
 import com.baba.back.content.repository.ContentRepository;
@@ -15,17 +19,22 @@ import com.baba.back.relation.repository.RelationRepository;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.Map;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 public class ContentAcceptanceTest extends AcceptanceTest {
 
     public static final String BASE_PATH = "/api/album";
+    public static final String VALID_URL = "http://test";
 
     @Autowired
     private TokenProvider tokenProvider;
@@ -41,6 +50,9 @@ public class ContentAcceptanceTest extends AcceptanceTest {
 
     @Autowired
     private ContentRepository contentRepository;
+
+    @MockBean
+    private AmazonS3 amazonS3;
 
     @AfterEach
     void tearDown() {
@@ -70,9 +82,11 @@ public class ContentAcceptanceTest extends AcceptanceTest {
     }
 
     @Test
-    void 컨텐츠를_생성한다() {
+    void S3에_파일_업로드_실패시_500을_던진다() {
         // given
         final String token = tokenProvider.createToken(멤버1.getId());
+
+        given(amazonS3.putObject(any())).willThrow(AmazonServiceException.class);
 
         memberRepository.save(멤버1);
         babyRepository.save(아기1);
@@ -81,7 +95,35 @@ public class ContentAcceptanceTest extends AcceptanceTest {
         // when
         final ExtractableResponse<Response> response = RestAssured.given()
                 .headers(Map.of("Authorization", "Bearer " + token))
-                .multiPart("photo", "test_file.jpg", "Something".getBytes(), "image/jpeg")
+                .multiPart("photo", "test_file.jpg", "Something".getBytes(), MediaType.IMAGE_PNG_VALUE)
+                .multiPart("date", LocalDate.of(2023, 1, 25).toString())
+                .multiPart("title", "제목")
+                .multiPart("cardStyle", "card_basic_1")
+                .when()
+                .post(Paths.get(BASE_PATH, 아기1.getId()).toString())
+                .then()
+                .log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    @Test
+    void 컨텐츠를_생성한다() throws MalformedURLException {
+        // given
+        final String token = tokenProvider.createToken(멤버1.getId());
+
+        memberRepository.save(멤버1);
+        babyRepository.save(아기1);
+        relationRepository.save(관계1);
+
+        given(amazonS3.getUrl(any(String.class), any(String.class))).willReturn(new URL(VALID_URL));
+
+        // when
+        final ExtractableResponse<Response> response = RestAssured.given()
+                .headers(Map.of("Authorization", "Bearer " + token))
+                .multiPart("photo", "test_file.jpg", "Something".getBytes(), MediaType.IMAGE_PNG_VALUE)
                 .multiPart("date", LocalDate.of(2023, 1, 25).toString())
                 .multiPart("title", "제목")
                 .multiPart("cardStyle", "card_basic_1")
