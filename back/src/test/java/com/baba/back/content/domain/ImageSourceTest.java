f5fd079fa0c803d5b51dac99ce183582fe74bb7a
JunHyeongKim73 <rla5764v@gmail.com>
2023-02-05 14:16:09 +0900

    test: `ImageSourceTest` 변경

--- back/src/test/java/com/baba/back/content/domain/ImageSourceTest.java
+++ back/src/test/java/com/baba/back/content/domain/ImageSourceTest.java
@@ -3,22 +3,20 @@ package com.baba.back.content.domain;
 import com.baba.back.content.domain.content.ImageSource;
 import com.baba.back.content.exception.ImageSourceBadRequestException;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.api.Test;
 
 class ImageSourceTest {
 
-    @ParameterizedTest
-    @NullSource
-    void ImageSource는_null일수_없다(String imageSource) {
+    @Test
+    void ImageSource는_null일수_없다() {
+        final String imageSource = null;
         Assertions.assertThatThrownBy(() -> new ImageSource(imageSource))
                 .isInstanceOf(ImageSourceBadRequestException.class);
     }
 
-    @ParameterizedTest
-    @EmptySource
-    void ImageSource는_빈값일수_있다(String imageSource) {
+    @Test
+    void ImageSource는_빈값일수_있다() {
+        final String imageSource = "";
         Assertions.assertThatCode(() -> new ImageSource(imageSource))
                 .doesNotThrowAnyException();
     }
