bde55395d02174cfcade887749fe7440491e8cb5
JunHyeongKim73 <rla5764v@gmail.com>
2023-02-05 14:05:13 +0900

    test: `ImageFile` 테스트 수정

--- back/src/test/java/com/baba/back/content/domain/ImageFileTest.java
+++ back/src/test/java/com/baba/back/content/domain/ImageFileTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.baba.back.content.exception.ImageFileBadRequestException;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,7 +14,8 @@ class ImageFileTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"text/plain", "text/html", "application/octet-stream", "image/gif"})
-    void 이미지가_아닌_파일을_받으면_예외를_던진다(String contentType) {
+    @NullAndEmptySource
+    void 유효하지_않은_파일을_받으면_예외를_던진다(String contentType) {
         // given
         MultipartFile mockFile = new MockMultipartFile("photo", "file.png", contentType,
                 "Spring Framework".getBytes());
