5204e711cd56f80f4a7adecbf0dd6babdac5e1aa
JunHyeongKim73 <rla5764v@gmail.com>
2023-02-12 16:25:52 +0900

    test: 테스트 오류 수정

--- back/src/test/java/com/baba/back/common/domain/BaseEntityTest.java
+++ back/src/test/java/com/baba/back/common/domain/BaseEntityTest.java
@@ -1,11 +1,19 @@
 package com.baba.back.common.domain;
 
 import static com.baba.back.fixture.DomainFixture.멤버1;
+import static com.baba.back.fixture.DomainFixture.아기1;
 import static com.baba.back.fixture.DomainFixture.컨텐츠;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.baba.back.baby.domain.Baby;
+import com.baba.back.baby.repository.BabyRepository;
 import com.baba.back.content.domain.Like;
+import com.baba.back.content.domain.content.Content;
+import com.baba.back.content.repository.ContentRepository;
 import com.baba.back.content.repository.LikeRepository;
+import com.baba.back.oauth.domain.member.Member;
+import com.baba.back.oauth.repository.MemberRepository;
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,19 +22,30 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 class BaseEntityTest {
 
     @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private BabyRepository babyRepository;
+
+    @Autowired
+    private ContentRepository contentRepository;
+
+    @Autowired
     private LikeRepository likeRepository;
 
     @Test
     void 생성시간과_업데이트시간을_확인한다() {
         // given
-        final Like like = Like.builder()
-                .member(멤버1)
-                .content(컨텐츠)
-                .build();
+        final LocalDate now = LocalDate.now();
+        final Member savedMember = memberRepository.save(멤버1);
+        final Baby savedBaby = babyRepository.save(아기1);
+        final Content savedContent = contentRepository.save(
+                new Content("제목", now, now, "card_basic_1", savedBaby, savedMember));
 
-        likeRepository.save(like);
+        // when
+        final Like like = likeRepository.save(new Like(savedMember, savedContent));
 
-        // when & then
+        // then
         assertThat(like.getCreatedDate()).isNotNull();
         assertThat(like.getUpdatedDate()).isNotNull();
     }
--- back/src/test/java/com/baba/back/content/repository/LikeRepositoryTest.java
+++ back/src/test/java/com/baba/back/content/repository/LikeRepositoryTest.java
@@ -2,15 +2,15 @@ package com.baba.back.content.repository;
 
 import static com.baba.back.fixture.DomainFixture.멤버1;
 import static com.baba.back.fixture.DomainFixture.아기1;
-import static com.baba.back.fixture.DomainFixture.좋아요;
-import static com.baba.back.fixture.DomainFixture.컨텐츠;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.baba.back.baby.domain.Baby;
 import com.baba.back.baby.repository.BabyRepository;
 import com.baba.back.content.domain.Like;
 import com.baba.back.content.domain.content.Content;
 import com.baba.back.oauth.domain.member.Member;
 import com.baba.back.oauth.repository.MemberRepository;
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -33,13 +33,15 @@ class LikeRepositoryTest {
     @Test
     void 멤버와_컨텐츠로_좋아요를_조회한다() {
         // given
-        final Member member = memberRepository.save(멤버1);
-        babyRepository.save(아기1);
-        final Content content = contentRepository.save(컨텐츠);
-        final Like savedLike = likeRepository.save(좋아요);
+        final LocalDate now = LocalDate.now();
+        final Member savedMember = memberRepository.save(멤버1);
+        final Baby savedBaby = babyRepository.save(아기1);
+        final Content savedContent = contentRepository.save(
+                new Content("제목", now, now, "card_basic_1", savedBaby, savedMember));
+        final Like savedLike = likeRepository.save(new Like(savedMember, savedContent));
 
         // when
-        final Like findLike = likeRepository.findByMemberAndContent(member, content).orElseThrow();
+        final Like findLike = likeRepository.findByMemberAndContent(savedMember, savedContent).orElseThrow();
 
         // then
         assertThat(savedLike).isEqualTo(findLike);
