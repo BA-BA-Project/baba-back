f7a4d58a84e4bf02a06952ed1010681933b114f8
JunHyeongKim73 <rla5764v@gmail.com>
2023-02-12 15:22:26 +0900

    feat: `findByContentDateAndBaby` 메소드 구현

---
+++ back/src/test/java/com/baba/back/content/repository/ContentRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.baba.back.content.repository;
+
+import static com.baba.back.fixture.DomainFixture.아기1;
+import static com.baba.back.fixture.DomainFixture.컨텐츠;
+
+import com.baba.back.baby.domain.Baby;
+import com.baba.back.baby.repository.BabyRepository;
+import com.baba.back.content.domain.content.Content;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class ContentRepositoryTest {
+    @Autowired
+    private BabyRepository babyRepository;
+
+    @Autowired
+    private ContentRepository contentRepository;
+
+    @Test
+    void 날짜와_아기로_컨텐츠를_조회할_수_있다() {
+        // given
+        final Baby baby = babyRepository.save(아기1);
+        final Content savedContent = contentRepository.save(컨텐츠);
+
+        // when
+        final Content foundContent = contentRepository.findByContentDateAndBaby(savedContent.getContentDate(), baby)
+                .orElseThrow();
+
+        // then
+        Assertions.assertThat(foundContent).isEqualTo(savedContent);
+    }
+}
