0d87f6e3606df487aaa38551b6461736b0276258
Kim Jun Hyeong <56298375+JunHyeongKim73@users.noreply.github.com>
2023-02-13 16:01:12 +0900

    chore: deploy.sh 수정

--- back/deploy.sh
+++ back/deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ec2-user/baba-was
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/zip/*.jar $REPOSITORY
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
