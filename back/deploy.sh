ce2bb4bc0e33b295493546c2dc6f20559b26a9a9
JunHyeongKim73 <rla5764v@gmail.com>
2023-02-14 15:56:30 +0900

    chore: LAST OF LAST

--- back/deploy.sh
+++ back/deploy.sh
@@ -11,7 +11,7 @@ if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  sudo kill -15 $CURRENT_PID
   sleep 5
 fi
 
@@ -29,9 +29,6 @@ echo "> JAR Name: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
-
-echo "> logs 폴더 삭제"
-rm -r $REPOSITORY/logs
 
 echo "> $JAR_NAME 실행"
 
