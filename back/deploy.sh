4480b70ea1550f7917f78a484a9f3c4e3bed8d02
JunHyeongKim73 <rla5764v@gmail.com>
2023-02-19 22:07:20 +0900

    chore: deploy.sh 수정

--- back/deploy.sh
+++ back/deploy.sh
@@ -44,7 +44,7 @@ else
   mv $REPOSITORY $REPOSITORY_PRD/zip
 
   echo "> 현재 구동 중인 애플리케이션 pid 확인"
-  CURRENT_PID=$(pgrep -경fl prd-APP.jar | awk '{print $1}')
+  CURRENT_PID=$(pgrep -fl prd-APP.jar | awk '{print $1}')
 
   if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
