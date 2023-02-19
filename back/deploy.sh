47cc126191dcce1f06ed56521ae928cde9d84879
Kim Jun Hyeong <56298375+JunHyeongKim73@users.noreply.github.com>
2023-02-19 22:27:05 +0900

    배포/개발 서버 cd를 구축한다. (#39)
    * refactor: cd.yml 파일 분리
    
    * refactor: deploy.sh 파일 변경
    
    * refactor: dev 프로파일 생성
    
    * refactor: dev 프로파일용 로그 설정
    
    * refactor: dev 브랜치에 merge 될 때 cd가 수행되도록 수정
    
    * refactor: 배포 그룹 수정
    
    * refactor: `logback.xml` 코드 중복 해결
    
    * refactor: `application.yml` 수정
    
    * chore: deploy.sh 수정

--- back/deploy.sh
+++ back/deploy.sh
@@ -1,27 +1,67 @@
 #! /bin/bash
 
 REPOSITORY=/home/ec2-user/baba-was/zip
-BUILD_FILE_REPOSITORY=$REPOSITORY/build/libs
-
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl back-0.0.1-SNAPSHOT.jar | awk '{print $1}')
-
-if [ -z "$CURRENT_PID" ]; then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-  echo "> kill -15 $CURRENT_PID"
-  sudo kill -15 $CURRENT_PID
-  sleep 5
-fi
+REPOSITORY_DEV=/home/ec2-user/baba-was/dev
+REPOSITORY_PRD=/home/ec2-user/baba-was/prd
 
 echo "> 환경변수 불러오기"
 source ~/.bashrc
 
-JAR_NAME=$(ls -tr $BUILD_FILE_REPOSITORY/*.jar | tail -n 1)
-echo "> JAR Name: $JAR_NAME"
+if [ "$DEPLOYMENT_GROUP_NAME" == "baba-was-dev" ]; then
+  echo "> 기존의 dev/zip 폴더 삭제"
+  rm -r $REPOSITORY_DEV/zip
 
-echo "> $JAR_NAME 에 실행권한 추가"
-chmod +x $JAR_NAME
+  echo "> 설치된 프로젝트 폴더 이동"
+  mv $REPOSITORY $REPOSITORY_DEV/zip
 
-echo "> $JAR_NAME 실행"
-nohup java -jar -Dspring.profiles.active=prd $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+  echo "> 현재 구동 중인 애플리케이션 pid 확인"
+  CURRENT_PID=$(pgrep -fl dev-APP.jar | awk '{print $1}')
+
+  if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  else
+    echo "> kill -15 $CURRENT_PID"
+    sudo kill -15 $CURRENT_PID
+    sleep 5
+  fi
+
+  OLD_JAR_NAME=$(ls -tr $REPOSITORY_DEV/zip/build/libs/*.jar | tail -n 1)
+  JAR_NAME=$REPOSITORY_DEV/zip/build/libs/dev-APP.jar
+  mv $OLD_JAR_NAME $JAR_NAME
+  echo "> JAR Name 변경: $OLD_JAR_NAME >> $JAR_NAME"
+
+  echo "> $JAR_NAME 에 실행권한 추가"
+  chmod +x $JAR_NAME
+
+  echo "> $JAR_NAME 실행"
+  nohup java -jar -Dspring.profiles.active=dev $JAR_NAME > $REPOSITORY_DEV/nohup.out 2>&1 &
+
+else
+  echo "> 기존의 prd/zip 폴더 삭제"
+  rm -r $REPOSITORY_PRD/zip
+
+  echo "> 설치된 프로젝트 폴더 이동"
+  mv $REPOSITORY $REPOSITORY_PRD/zip
+
+  echo "> 현재 구동 중인 애플리케이션 pid 확인"
+  CURRENT_PID=$(pgrep -fl prd-APP.jar | awk '{print $1}')
+
+  if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  else
+    echo "> kill -15 $CURRENT_PID"
+    sudo kill -15 $CURRENT_PID
+    sleep 5
+  fi
+
+  OLD_JAR_NAME=$(ls -tr $REPOSITORY_PRD/zip/build/libs/*.jar | tail -n 1)
+  JAR_NAME=$REPOSITORY_PRD/zip/build/libs/prd-APP.jar
+  mv $OLD_JAR_NAME $JAR_NAME
+  echo "> JAR Name 변경: $OLD_JAR_NAME >> $JAR_NAME"
+
+  echo "> $JAR_NAME 에 실행권한 추가"
+  chmod +x $JAR_NAME
+
+  echo "> $JAR_NAME 실행"
+  nohup java -jar -Dspring.profiles.active=prd $JAR_NAME > $REPOSITORY_PRD/nohup.out 2>&1 &
+fi
