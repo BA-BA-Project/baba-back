47eecf06e599f96c9c6a5023052cd72b3385d4ab
JunHyeongKim73 <rla5764v@gmail.com>
2023-02-24 13:52:09 +0900

    test: `ContentDateTest` 수정

--- back/src/test/java/com/baba/back/content/domain/content/ContentDateTest.java
+++ back/src/test/java/com/baba/back/content/domain/content/ContentDateTest.java
@@ -6,25 +6,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import com.baba.back.content.exception.ContentDateBadRequestException;
 import java.time.LocalDate;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class ContentDateTest {
+    private final static LocalDate now = LocalDate.now();
 
-    public static Stream<Arguments> invalidContentDateAndNow() {
-        final LocalDate now = LocalDate.now();
+    public static Stream<Arguments> nullLocalDate() {
         return Stream.of(
-                Arguments.of(null, null, null),
-                Arguments.of(now.plusDays(1), now, now)
-        );
-    }
-
-    public static Stream<Arguments> invalidContentDateAndBirthday() {
-        final LocalDate now = LocalDate.now();
-        return Stream.of(
-                Arguments.of(null, null, null),
-                Arguments.of(now.minusYears(2).minusDays(1), now, now)
+                Arguments.of(null, now, now),
+                Arguments.of(now, null, now),
+                Arguments.of(now, now, null)
         );
     }
 
@@ -37,16 +31,29 @@ class ContentDateTest {
     }
 
     @ParameterizedTest
-    @MethodSource("invalidContentDateAndNow")
-    void contentDate는_now보다_미래일수_없다(LocalDate contentDate, LocalDate now, LocalDate baseDate) {
+    @MethodSource("nullLocalDate")
+    void null이_존재하면_contentDate를_생성할_수_없다(LocalDate contentDate, LocalDate now, LocalDate baseDate) {
         assertThatThrownBy(() -> ContentDate.of(contentDate, now, baseDate))
                 .isInstanceOf(ContentDateBadRequestException.class);
     }
 
-    @ParameterizedTest
-    @MethodSource("invalidContentDateAndBirthday")
-    void contentDate는_birthday의_2년전까지_유효하다(LocalDate contentDate, LocalDate now, LocalDate baseDate) {
-        assertThatThrownBy(() -> ContentDate.of(contentDate, now, baseDate))
+    @Test
+    void contentDate는_now보다_미래일수_없다() {
+        // given
+        final LocalDate contentDate = now.plusDays(1);
+
+        // when & then
+        assertThatThrownBy(() -> ContentDate.of(contentDate, now, now))
+                .isInstanceOf(ContentDateBadRequestException.class);
+    }
+
+    @Test
+    void contentDate는_birthday의_2년전까지_유효하다() {
+        // given
+        final LocalDate contentDate = now.minusYears(2).minusDays(1);
+
+        // when & then
+        assertThatThrownBy(() -> ContentDate.of(contentDate, now, now))
                 .isInstanceOf(ContentDateBadRequestException.class);
     }
 
