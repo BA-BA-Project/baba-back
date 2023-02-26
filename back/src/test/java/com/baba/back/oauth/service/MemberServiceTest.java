19ff48c51b2906e53477e16fb823b8b49f4e40a0
JunHyeongKim73 <rla5764v@gmail.com>
2023-02-26 16:45:03 +0900

    refactor: `MemberService` 수정

--- back/src/test/java/com/baba/back/oauth/service/MemberServiceTest.java
+++ back/src/test/java/com/baba/back/oauth/service/MemberServiceTest.java
@@ -1,28 +1,31 @@
 package com.baba.back.oauth.service;
 
-import static com.baba.back.fixture.DomainFixture.이미_회원가입한_유저1;
-import static com.baba.back.fixture.DomainFixture.회원가입_안한_유저1;
+import static com.baba.back.fixture.DomainFixture.관계1;
+import static com.baba.back.fixture.DomainFixture.관계2;
+import static com.baba.back.fixture.DomainFixture.멤버1;
+import static com.baba.back.fixture.DomainFixture.아기1;
+import static com.baba.back.fixture.DomainFixture.아기2;
 import static com.baba.back.fixture.RequestFixture.멤버_가입_요청;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 
+import com.baba.back.baby.domain.Baby;
 import com.baba.back.baby.domain.IdConstructor;
 import com.baba.back.baby.repository.BabyRepository;
 import com.baba.back.oauth.domain.Picker;
 import com.baba.back.oauth.domain.member.IconColor;
-import com.baba.back.oauth.dto.MemberJoinRequest;
-import com.baba.back.oauth.dto.MemberJoinResponse;
-import com.baba.back.oauth.exception.JoinedMemberBadRequestException;
-import com.baba.back.oauth.exception.JoinedMemberNotFoundException;
-import com.baba.back.oauth.repository.JoinedMemberRepository;
+import com.baba.back.oauth.domain.member.Member;
+import com.baba.back.oauth.dto.MemberSignUpRequest;
+import com.baba.back.oauth.dto.MemberSignUpResponse;
+import com.baba.back.oauth.exception.MemberBadRequestException;
 import com.baba.back.oauth.repository.MemberRepository;
+import com.baba.back.relation.domain.Relation;
 import com.baba.back.relation.repository.RelationRepository;
-import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -41,9 +44,6 @@ class MemberServiceTest {
     private MemberRepository memberRepository;
 
     @Mock
-    private JoinedMemberRepository joinedMemberRepository;
-
-    @Mock
     private BabyRepository babyRepository;
 
     @Mock
@@ -55,41 +55,52 @@ class MemberServiceTest {
     @Mock
     private IdConstructor idConstructor;
 
-    @Test
-    void 로그인을_하지_않은_멤버는_회원가입을_할_수_없다() {
-        // given
-        final String invalidMemberId = "memberId";
-        given(joinedMemberRepository.findById(invalidMemberId)).willReturn(Optional.empty());
+    @Mock
+    private AccessTokenProvider accessTokenProvider;
 
-        // when & then
-        Assertions.assertThatThrownBy(() -> memberService.join(new MemberJoinRequest(), invalidMemberId))
-                .isInstanceOf(JoinedMemberNotFoundException.class);
-    }
+    @Mock
+    private RefreshTokenProvider refreshTokenProvider;
 
     @Test
     void 이미_회원가입한_멤버는_회원가입할_수_없다() {
         // given
-        given(joinedMemberRepository.findById(anyString())).willReturn(Optional.of(이미_회원가입한_유저1));
+        final String memberId = "memberId";
+
+        given(memberRepository.existsById(memberId)).willReturn(true);
 
         // when & then
-        Assertions.assertThatThrownBy(() -> memberService.join(new MemberJoinRequest(), "memberId"))
-                .isInstanceOf(JoinedMemberBadRequestException.class);
+        Assertions.assertThatThrownBy(() -> memberService.signUp(new MemberSignUpRequest(), memberId))
+                .isInstanceOf(MemberBadRequestException.class);
     }
 
     @Test
     void 회원가입을_진행한다() {
         // given
-        given(joinedMemberRepository.findById(anyString())).willReturn(Optional.of(회원가입_안한_유저1));
-        given(picker.pick(anyList())).willReturn(IconColor.COLOR_1);
+        final String memberId = "memberId";
+        final String accessToken = "accessToken";
+        final String refreshToken = "refreshToken";
 
-        final MemberJoinResponse response = memberService.join(멤버_가입_요청, "memberId");
+        given(memberRepository.existsById(memberId)).willReturn(false);
+        given(picker.pick(anyList())).willReturn(IconColor.COLOR_1);
+        given(memberRepository.save(any(Member.class))).willReturn(멤버1);
+        given(idConstructor.createId()).willReturn(아기1.getId(), 아기2.getId());
+        given(babyRepository.save(any(Baby.class))).willReturn(아기1, 아기2);
+        given(relationRepository.save(any(Relation.class))).willReturn(관계1, 관계2);
+        given(accessTokenProvider.createToken(memberId)).willReturn(accessToken);
+        given(refreshTokenProvider.createToken(memberId)).willReturn(refreshToken);
+
+        // when
+        final MemberSignUpResponse response = memberService.signUp(멤버_가입_요청, memberId);
 
         //then
         then(memberRepository).should(times(1)).save(any());
-        then(idConstructor).should(times(1)).createId();
+        then(idConstructor).should(times(2)).createId();
         then(babyRepository).should(times(2)).save(any());
         then(relationRepository).should(times(2)).save(any());
 
-        assertThat(response.result()).isNotBlank();
+        assertAll(
+                () -> assertThat(response.accessToken()).isEqualTo(accessToken),
+                () -> assertThat(response.refreshToken()).isEqualTo(refreshToken)
+        );
     }
 }
