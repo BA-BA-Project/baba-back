9b50f8880065974aa1c75c9f4d8a1101df7eeda1
seong-wooo <seongwoo0513@uos.ac.kr>
2023-02-27 19:04:15 +0900

    refactor: value object의 필드 값을 value로 변경

--- back/src/main/java/com/baba/back/oauth/domain/member/IconColor.java
+++ back/src/main/java/com/baba/back/oauth/domain/member/IconColor.java
@@ -4,20 +4,18 @@ import com.baba.back.oauth.domain.Picker;
 import com.baba.back.oauth.exception.IconColorBadRequestException;
 import java.util.Arrays;
 import java.util.List;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Getter
 @RequiredArgsConstructor
 public enum IconColor {
     COLOR_1("FFAEBA"), COLOR_2("FF8698"), COLOR_3("FFE3C8"), COLOR_4("FFD2A7"), COLOR_5("FFD400"), COLOR_6("9ED883"),
     COLOR_7("30BE9B"), COLOR_8("81E0D5"), COLOR_9("5BD2FF"), COLOR_10("97BEFF"), COLOR_11("98A2FF"), COLOR_12("BFA1FF");
 
-    private final String color;
+    private final String value;
 
     public static IconColor from(String color) {
         return Arrays.stream(IconColor.values())
-                .filter(iconColor -> iconColor.getColor().equals(color))
+                .filter(iconColor -> iconColor.value.equals(color))
                 .findAny()
                 .orElseThrow(() -> new IconColorBadRequestException(color + " 는 잘못된 IconColor 입니다."));
     }
