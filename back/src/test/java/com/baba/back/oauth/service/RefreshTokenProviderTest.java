f5a5aed8e502b0d3ceb4911d8e015bca495837be
JunHyeongKim73 <rla5764v@gmail.com>
2023-03-01 21:29:20 +0900

    test: `RefreshTokenProviderTest` clock을 통해 시간여행 하도록 변경

--- back/src/test/java/com/baba/back/oauth/service/RefreshTokenProviderTest.java
+++ back/src/test/java/com/baba/back/oauth/service/RefreshTokenProviderTest.java
@@ -1,56 +1,46 @@
 package com.baba.back.oauth.service;
 
-import static org.mockito.BDDMockito.given;
-
 import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 class RefreshTokenProviderTest {
 
     private static final String SECRET_KEY = "a".repeat(100);
-    private static final Long VALIDITY_MILLISECONDS = 86400000L;
+    private static final Long VALIDITY_MILLISECONDS = 172800000L;
     private RefreshTokenProvider refreshTokenProvider;
-
-    @Mock
-    private Clock clock;
 
     @Test
     void 만료기간이_하루_이하로_남으면_true를_반환한다() {
         // given
         final String memberId = "memberId";
-        final Clock now = Clock.systemDefaultZone();
-
-        given(clock.instant()).willReturn(now.instant());
-        given(clock.getZone()).willReturn(now.getZone());
-
+        final LocalDateTime now = LocalDateTime.of(2023, 3, 1, 20, 20, 0);
+        final Clock clock = Clock.fixed(now.atZone(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault());
         refreshTokenProvider = new RefreshTokenProvider(SECRET_KEY, VALIDITY_MILLISECONDS, clock);
-
         final String refreshToken = refreshTokenProvider.createToken(memberId);
+
+        final Clock timeTravelClock = Clock.fixed(now.plusDays(1)
+                .atZone(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault());
+        refreshTokenProvider = new RefreshTokenProvider(SECRET_KEY, VALIDITY_MILLISECONDS, timeTravelClock);
 
         // when & then
         Assertions.assertThat(refreshTokenProvider.isExpiringSoon(refreshToken)).isTrue();
     }
 
-    @ParameterizedTest
-    @ValueSource(longs = {86410000L, 172800000L})
-    void 만료기간이_하루보다_많이_남으면_false를_반환한다(Long milliSeconds) {
+    @Test
+    void 만료기간이_하루보다_많이_남으면_false를_반환한다() {
         // given
         final String memberId = "memberId";
-        final Clock now = Clock.systemDefaultZone();
-
-        given(clock.instant()).willReturn(now.instant());
-        given(clock.getZone()).willReturn(now.getZone());
-
-        refreshTokenProvider = new RefreshTokenProvider(SECRET_KEY, milliSeconds, clock);
-
+        final LocalDateTime now = LocalDateTime.of(2023, 3, 1, 20, 20, 0);
+        final Clock clock = Clock.fixed(now.atZone(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault());
+        refreshTokenProvider = new RefreshTokenProvider(SECRET_KEY, VALIDITY_MILLISECONDS, clock);
         final String refreshToken = refreshTokenProvider.createToken(memberId);
+
+        final Clock timeTravelClock = Clock.fixed(now.plusDays(1).minusSeconds(1)
+                .atZone(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault());
+        refreshTokenProvider = new RefreshTokenProvider(SECRET_KEY, VALIDITY_MILLISECONDS, timeTravelClock);
 
         // when & then
         Assertions.assertThat(refreshTokenProvider.isExpiringSoon(refreshToken)).isFalse();
