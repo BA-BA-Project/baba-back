5a3447593d0b4eb7ffe7ad60ce9600d7ec2223ab
SWpark <seongwoo0513@uos.ac.kr>
2023-03-01 12:15:53 +0900

    사용자 정보 조회 API 를 구현한다 (#62)
    * refactor: value object의 필드 값을 value로 변경
    
    * feat: 사용자 정보 조회 API 추가 및 테스트
    
    * refactor: reformat code 적용
    
    * refactor: UnAuthorizedResponse annotation 추가

--- back/src/main/java/com/baba/back/oauth/controller/MemberController.java
+++ back/src/main/java/com/baba/back/oauth/controller/MemberController.java
@@ -1,13 +1,16 @@
 package com.baba.back.oauth.controller;
 
+import com.baba.back.oauth.dto.MemberResponse;
 import com.baba.back.oauth.dto.MemberSignUpRequest;
 import com.baba.back.oauth.dto.MemberSignUpResponse;
 import com.baba.back.oauth.service.MemberService;
+import com.baba.back.oauth.support.Login;
 import com.baba.back.oauth.support.SignUp;
 import com.baba.back.swagger.BadRequestResponse;
 import com.baba.back.swagger.CreatedResponse;
 import com.baba.back.swagger.IntervalServerErrorResponse;
 import com.baba.back.swagger.NotFoundResponse;
+import com.baba.back.swagger.OkResponse;
 import com.baba.back.swagger.UnAuthorizedResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,6 +18,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +40,15 @@ public class MemberController {
     public ResponseEntity<MemberSignUpResponse> joinMember(@RequestBody @Valid MemberSignUpRequest request,
                                                            @SignUp String memberId) {
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.signUp(request, memberId));
+    }
+
+    @Operation(summary = "멤버 정보 조회 요청")
+    @OkResponse
+    @UnAuthorizedResponse
+    @NotFoundResponse
+    @IntervalServerErrorResponse
+    @GetMapping("/members")
+    public ResponseEntity<MemberResponse> findMember(@Login String memberId) {
+        return ResponseEntity.ok(memberService.findMember(memberId));
     }
 }
