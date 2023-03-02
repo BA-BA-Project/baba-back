5491120c7ba26b1a0f198c9514f7fd1aabe8dc29
seong-wooo <seongwoo0513@uos.ac.kr>
2023-03-02 20:46:46 +0900

    refactor: 토큰 값 변경 메소드 추가 및 테스트

--- back/src/test/java/com/baba/back/oauth/repository/TokenRepositoryTest.java
+++ back/src/test/java/com/baba/back/oauth/repository/TokenRepositoryTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.baba.back.oauth.domain.member.Member;
 import com.baba.back.oauth.domain.token.Token;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -23,15 +24,15 @@ class TokenRepositoryTest {
     void 멤버와_토큰값쌍이_DB에_존재하면_true를_반환한다() {
         // given
         final Member member = memberRepository.save(멤버1);
-        final String token = "token";
+        final String value = "token";
 
         tokenRepository.save(Token.builder()
                 .member(member)
-                .value(token)
+                .value(value)
                 .build());
 
         // when
-        final boolean exists = tokenRepository.existsByMemberAndValue(member, token);
+        final boolean exists = tokenRepository.existsByMemberAndValue(member, value);
 
         // then
         assertThat(exists).isTrue();
@@ -50,18 +51,38 @@ class TokenRepositoryTest {
     void 멤버와_토큰값쌍이_DB에_존재하지_않으면_false를_반환한다() {
         // given
         final Member member = memberRepository.save(멤버1);
-        final String validToken = "validToken";
-        final String invalidToken = "invalidToken";
+        final String value = "validToken";
+        final String invalidValue = "invalidToken";
 
         tokenRepository.save(Token.builder()
                 .member(member)
-                .value(validToken)
+                .value(value)
                 .build());
 
         // when
-        final boolean exists = tokenRepository.existsByMemberAndValue(member, invalidToken);
+        final boolean exists = tokenRepository.existsByMemberAndValue(member, invalidValue);
 
         // then
         assertThat(exists).isFalse();
+    }
+
+    @Test
+    void 멤버에_대한_refreshToken이_저장되어있는지_확인한다() {
+        // given
+        final String value = "validToken";
+
+        final Member member = memberRepository.save(멤버1);
+        final Token token = tokenRepository.save(
+                Token.builder()
+                        .member(member)
+                        .value(value)
+                        .build()
+        );
+
+        // when
+        final Token savedToken = tokenRepository.findByMember(member).orElseThrow();
+
+        // then
+        Assertions.assertThat(token).isEqualTo(savedToken);
     }
 }
