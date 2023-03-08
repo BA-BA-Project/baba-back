ea962a84f3386ce51a83341e941357167fef3f72
JunHyeongKim73 <rla5764v@gmail.com>
2023-03-08 11:17:51 +0900

    refactor: `InviteCode` 수정

--- back/src/main/java/com/baba/back/invitation/domain/InviteCode.java
+++ back/src/main/java/com/baba/back/invitation/domain/InviteCode.java
@@ -11,11 +11,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class InviteCode {
     public static final int INVITE_CODE_LENGTH = 6;
-    public static final String UPPERCASE_AND_NUMBER = "[A-Z0-9]{6}";
+    public static final String ALLOWED_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private String value;
 
     public InviteCode(String value) {
         validateNull(value);
+        validateLength(value);
         validateCode(value);
         this.value = value;
     }
@@ -26,13 +27,18 @@ public class InviteCode {
         }
     }
 
-    private void validateCode(String value) {
+    private void validateLength(String value) {
         if (value.length() != INVITE_CODE_LENGTH) {
             throw new InviteCodeBadRequestException("초대코드는 " + INVITE_CODE_LENGTH + "자여야 합니다.");
         }
+    }
 
-        if (!value.matches(UPPERCASE_AND_NUMBER)) {
-            throw new InviteCodeBadRequestException("{" + value + "}는 올바르지 않은 초대코드입니다.");
+    private void validateCode(String value) {
+        for (int i = 0; i < value.length(); i++) {
+            char c = value.charAt(i);
+            if (ALLOWED_CHARS.indexOf(c) == -1) {
+                throw new InviteCodeBadRequestException("{" + value + "}는 올바르지 않은 초대코드입니다.");
+            }
         }
     }
 }
