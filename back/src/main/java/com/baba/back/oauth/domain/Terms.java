5aea00cd72344b9e5abd57932044b61bb33d1707
JunHyeongKim73 <rla5764v@gmail.com>
2023-03-09 21:02:52 +0900

    feat: `Terms` 클래스 구현

---
+++ back/src/main/java/com/baba/back/oauth/domain/Terms.java
@@ -0,0 +1,22 @@
+package com.baba.back.oauth.domain;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Terms {
+    TERMS_1(true, "이용약관 동의",
+            "https://sites.google.com/view/baba-agree/%EC%9D%B4%EC%9A%A9%EC%95%BD%EA%B4%80?authuser=1"),
+    TERMS_2(true, "개인정보 수집 및 이용 동의",
+            "https://sites.google.com/view/baba-agree/%EA%B0%9C%EC%9D%B8%EC%A0%95%EB%B3%B4%EC%B2%98%EB%A6%AC%EB%B0%A9%EC%B9%A8?authuser=1");
+
+    private final boolean required;
+    private final String name;
+    private final String url;
+
+    public static List<Terms> get() {
+        return List.of(Terms.values());
+    }
+}
