184a99e0f73b3694af461add615d875e41869d86
Kim Jun Hyeong <56298375+JunHyeongKim73@users.noreply.github.com>
2023-03-10 22:26:29 +0900

    약관 제공 API를 구현한다. (#79)
    * feat: `Terms` 클래스 구현
    
    * feat: `OAuthService` 약관 조회 기능 추가
    
    * feat: 약관 조회 API 구현
    
    * refactor: 불필요한 메소드 제거
    
    * refactor: static을 이용한 비용 절감
    
    * test: 테스트 수정
    
    * test: 테스트명 수정
    
    * refactor: 코드 라인 수정
    
    * refactor: 코드 라인 수정

--- back/src/main/java/com/baba/back/oauth/service/OAuthService.java
+++ back/src/main/java/com/baba/back/oauth/service/OAuthService.java
@@ -1,17 +1,22 @@
 package com.baba.back.oauth.service;
 
 import com.baba.back.oauth.OAuthClient;
+import com.baba.back.oauth.domain.Terms;
 import com.baba.back.oauth.domain.member.Member;
 import com.baba.back.oauth.domain.token.Token;
+import com.baba.back.oauth.dto.SearchTermsResponse;
 import com.baba.back.oauth.dto.SocialLoginResponse;
 import com.baba.back.oauth.dto.SocialTokenRequest;
+import com.baba.back.oauth.dto.TermsResponse;
 import com.baba.back.oauth.dto.TokenRefreshRequest;
 import com.baba.back.oauth.dto.TokenRefreshResponse;
+import com.baba.back.oauth.exception.MemberBadRequestException;
 import com.baba.back.oauth.exception.MemberNotFoundException;
 import com.baba.back.oauth.exception.TokenBadRequestException;
 import com.baba.back.oauth.repository.MemberRepository;
 import com.baba.back.oauth.repository.TokenRepository;
 import jakarta.transaction.Transactional;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,6 +26,11 @@ import org.springframework.stereotype.Service;
 @Transactional
 @RequiredArgsConstructor
 public class OAuthService {
+
+    private static final SearchTermsResponse SEARCH_TERMS_RESPONSE = new SearchTermsResponse(
+            Arrays.stream(Terms.values())
+                    .map(terms -> new TermsResponse(terms.isRequired(), terms.getName(), terms.getUrl()))
+                    .toList());
 
     private final OAuthClient oAuthClient;
     private final AccessTokenProvider accessTokenProvider;
@@ -41,14 +51,22 @@ public class OAuthService {
 
     private void saveRefreshToken(Member member, String refreshToken) {
         final Token token = tokenRepository.findByMember(member)
-                .orElseGet(
-                        () -> Token.builder()
-                                .member(member)
-                                .value(refreshToken)
-                                .build()
-                );
+                .orElseGet(() -> Token.builder().member(member).value(refreshToken).build());
         token.update(refreshToken);
         tokenRepository.save(token);
+    }
+
+    public SearchTermsResponse searchTerms(SocialTokenRequest request) {
+        final String memberId = oAuthClient.getMemberId(request.getSocialToken());
+        validateMember(memberId);
+
+        return SEARCH_TERMS_RESPONSE;
+    }
+
+    private void validateMember(String memberId) {
+        if (memberRepository.existsById(memberId)) {
+            throw new MemberBadRequestException("이미 회원가입된 유저는 약관을 조회할 수 없습니다.");
+        }
     }
 
     public TokenRefreshResponse refresh(TokenRefreshRequest request) {
@@ -72,9 +90,8 @@ public class OAuthService {
     }
 
     private Member findMember(String memberId) {
-        return memberRepository.findById(memberId).orElseThrow(
-                () -> new MemberNotFoundException(memberId + "에 해당하는 멤버가 존재하지 않습니다.")
-        );
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException(memberId + "에 해당하는 멤버가 존재하지 않습니다."));
     }
 
     private void validateToken(Member member, String refreshToken) {
