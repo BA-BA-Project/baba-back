a38d3776cd3ab2945a35d5d455298901d24f0475
seong-wooo <seongwoo0513@uos.ac.kr>
2023-03-10 23:06:42 +0900

    refactor: ContentRepository @Spy에서 @Mock으로 수정

--- back/src/test/java/com/baba/back/content/service/ContentServiceTest.java
+++ back/src/test/java/com/baba/back/content/service/ContentServiceTest.java
@@ -37,7 +37,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -46,7 +45,7 @@ class ContentServiceTest {
     public static final String MEMBER_ID = "1234";
     public static final String BABY_ID = "1234";
 
-    @Spy
+    @Mock
     private ContentRepository contentRepository;
 
     @Mock
