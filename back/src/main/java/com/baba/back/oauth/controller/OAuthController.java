81b355f7ee27d30fe4fe801dc95df8514d010bb2
Kim Jun Hyeong <56298375+JunHyeongKim73@users.noreply.github.com>
2023-03-13 01:00:42 +0900

    chore: API url 변경 (#84)

--- back/src/main/java/com/baba/back/oauth/controller/OAuthController.java
+++ back/src/main/java/com/baba/back/oauth/controller/OAuthController.java
@@ -18,6 +18,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +45,7 @@ public class OAuthController {
     @OkResponse
     @BadRequestResponse
     @IntervalServerErrorResponse
-    @PostMapping("/auth/login/terms")
+    @GetMapping("/auth/terms")
     public ResponseEntity<SearchTermsResponse> searchTerms(@RequestBody @Valid SocialTokenRequest tokenRequest) {
         return ResponseEntity.ok().body(oauthService.searchTerms(tokenRequest));
     }
