08c9e954e786c52fa42a0287e2d98cf0284c44a5
JunHyeongKim73 <rla5764v@gmail.com>
2023-03-14 01:51:39 +0900

    refactor: `Terms` enum 클래스 수정

--- back/src/test/java/com/baba/back/fixture/RequestFixture.java
+++ back/src/test/java/com/baba/back/fixture/RequestFixture.java
@@ -4,6 +4,7 @@ import static com.baba.back.content.domain.content.CardStyle.CARD_BASIC_1;
 
 import com.baba.back.baby.dto.BabyRequest;
 import com.baba.back.content.dto.CreateContentRequest;
+import com.baba.back.oauth.domain.Terms;
 import com.baba.back.oauth.dto.AgreeTermsRequest;
 import com.baba.back.oauth.dto.MemberSignUpRequest;
 import com.baba.back.oauth.dto.SocialTokenRequest;
@@ -14,10 +15,11 @@ import org.springframework.mock.web.MockMultipartFile;
 
 public class RequestFixture {
 
-    public static final TermsRequest 약관_요청_데이터1 = new TermsRequest("이용약관 동의", true);
-    public static final TermsRequest 약관_요청_데이터2 = new TermsRequest("개인정보 수집 및 이용 동의", true);
+    public static final TermsRequest 약관_요청_데이터1 = new TermsRequest(Terms.TERMS_1.getName(), true);
+    public static final TermsRequest 약관_요청_데이터2 = new TermsRequest(Terms.TERMS_2.getName(), true);
+    public static final TermsRequest 약관_요청_데이터3 = new TermsRequest(Terms.TERMS_3.getName(), true);
     public static final AgreeTermsRequest 약관_동의_요청_데이터 = new AgreeTermsRequest(
-            "socialToken", List.of(약관_요청_데이터1, 약관_요청_데이터2)
+            "socialToken", List.of(약관_요청_데이터1, 약관_요청_데이터2, 약관_요청_데이터3)
     );
 
     public static final BabyRequest 아기_생성_요청_데이터_1 = new BabyRequest("아기1", LocalDate.now());
--- back/src/test/java/com/baba/back/oauth/domain/TermsTest.java
+++ back/src/test/java/com/baba/back/oauth/domain/TermsTest.java
@@ -8,36 +8,40 @@ import org.junit.jupiter.api.Test;
 
 class TermsTest {
 
+    public static final int REQUIRED_TERMS_LENGTH = 2;
+    public static final int ALL_TERMS_LENGTH = Terms.values().length;
+
     @Test
-    void 약관의_개수가_다르면_false를_반환한다() {
+    void 모든_약관의_개수가_다르면_false를_반환한다() {
         // given & when
-        final boolean sameSize = Terms.isSameSize(Terms.values().length - 1);
+        final boolean sameSize = Terms.isSizeEqualToAllTerms(REQUIRED_TERMS_LENGTH);
 
         // then
         assertThat(sameSize).isFalse();
     }
 
     @Test
-    void 약관의_개수가_같으면_true를_반환한다() {
+    void 모든_약관의_개수가_같으면_true를_반환한다() {
         // given & when
-        final boolean sameSize = Terms.isSameSize(Terms.values().length);
+        final boolean sameSize = Terms.isSizeEqualToAllTerms(ALL_TERMS_LENGTH);
 
         // then
         assertThat(sameSize).isTrue();
     }
 
     @Test
-    void N번째_약관과_이름이_다르다면_예외를_던진다() {
-        assertThatThrownBy(() -> Terms.isRequiredTermsBy(0, Terms.TERMS_2.getName()))
+    void 요청받은_이름의_약관이_존재하지_않으면_예외를_던진다() {
+        // given
+        final String invalidTermsName = "존재하지 않은 약관";
+
+        // when & then
+        assertThatThrownBy(() -> Terms.findByName(invalidTermsName))
                 .isInstanceOf(TermsBadRequestException.class);
     }
 
     @Test
-    void N번째_약관과_이름이_같다면_약관의_필수여부를_반환한다() {
-        // given
-        final boolean requiredTerms = Terms.isRequiredTermsBy(0, Terms.TERMS_1.getName());
-
-        assertThat(requiredTerms).isEqualTo(Terms.TERMS_1.isRequired());
+    void 요청받은_이름의_약관이_존재하면_약관을_반환한다() {
+        assertThat(Terms.findByName(Terms.TERMS_2.getName())).isEqualTo(Terms.TERMS_2);
     }
 
 
