cd634be8b00afc62ed5c2d267c1a49f4fab11643
JunHyeongKim73 <rla5764v@gmail.com>
2023-06-06 11:54:56 +0900

    chore: 오류 수정

--- back/src/main/java/com/baba/back/content/repository/LikeRepository.java
+++ back/src/main/java/com/baba/back/content/repository/LikeRepository.java
@@ -7,10 +7,11 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Optional<Like> findByContentAndMember(Content content, Member member);
 
     @Query("select l from Like l where l.content = :content and l.deleted = false")
-    List<Like> findAllByContent(Content content);
+    List<Like> findAllByContent(@Param("content") Content content);
 }
